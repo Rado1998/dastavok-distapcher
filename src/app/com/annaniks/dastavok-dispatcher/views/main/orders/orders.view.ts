8730b3ff5b3250341eca1fb1844de6392412822a
6fb84fcd94 <rado.1998>
2018-10-31 17:50:28 +0400

    created orders module

---
+++ src/app/com/annaniks/dastavok-dispatcher/views/main/orders/orders.view.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'orders-view',
+    templateUrl: 'orders.view.html',
+    styleUrls: ['orders.view.scss']
+})
+export class OrdersView implements OnInit, OnDestroy {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+    ngOnDestroy() { }
+}
