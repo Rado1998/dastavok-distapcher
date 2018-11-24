36211782517d6c4e02f98acb495af92817079e64
rado <rado.poghosyan@annaniks.com>
2018-11-24 11:47:39 +0400

    changed orders page

--- src/app/com/annaniks/dastavok-dispatcher/components/loading/loading.component.ts
+++ src/app/com/annaniks/dastavok-dispatcher/components/loading/loading.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LoadingComponent implements OnInit {
     @Input('visiblity') public visiblity: boolean = false;
+    @Input('size') public spinnerSize:number = 150;
 
     constructor() { }
 
